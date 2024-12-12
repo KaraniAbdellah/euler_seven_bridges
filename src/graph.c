7f0f8393bb96ed146809566e4d0ebb1b66f01b7e
abdellah karani <abdellahkarani@gmail.com>
2024-12-12 07:31:29 +0100

    add init_graph

--- src/graph.c
+++ src/graph.c
@@ -3,3 +3,23 @@
 
 
 
+list *init_graph(int n) {
+	if (n == 0) return NULL;
+	list *graph = (list *) malloc(sizeof(list) * n);
+	return graph;
+}
+
+
+void add_node(int s, int d, list *graph) {
+	
+	if () {
+		
+	}
+	
+}
+
+
+
+
+
+
--- src/main.c
+++ src/main.c
@@ -8,6 +8,10 @@
 int main() {
 	
 	// create nodes of the graph
+	int n = 7;
+	list *graph = init_graph(n);
+	
+	
 	
 	
 	return 0;
